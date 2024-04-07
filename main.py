63efe6c0dc3650026bc08475ed0cb4a8f0b3f24a
bunvin <37735956+bunvin@users.noreply.github.com>
2024-04-07 05:48:08 +0000

    finished !. next, add updating database (followers are not updated)

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
-import game_data
 import random
+from replit import clear
 
-print("welcome to the higher lower game!!")
+import game_data
 
+print("welcome to the higher lower game!!\n")
 
-score = 0
-first_choice = random.choice(game_data.data)
-second_choice = random.choice(game_data.data)
+def new_game():
+  score = 0
+  game_over = False
+  first_choice = random.choice(game_data.data)
+  second_choice = random.choice(game_data.data)
+  
+  while not game_over:
+    print(f"Option A: {first_choice['name']},a {first_choice['description']} \nhas {first_choice['follower_count']}M followers.")
+    
+    print("\nVS.\n")
+    
+    print(f"Option B: {second_choice['name']}, a {second_choice['description']}\n")
+    
+    user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+      
+    if user_choice == "A":
+       if first_choice['follower_count'] > second_choice['follower_count']:
+         print("\nyou are correct !")
+         score += 1
+         print(f"your score is {score}\n")
+         first_choice = second_choice
+         second_choice = random.choice(game_data.data)
+       else:
+         print(f"\nyou are wrong ! \n{second_choice['name']} has {second_choice['follower_count']} followers")
+         print(f"your score is {score}")
+         game_over = True
+    
+    elif user_choice == "B":
+         if first_choice['follower_count'] < second_choice['follower_count']:
+           print("\nyou are correct !")
+           score += 1
+           print(f"your score is {score}\n")
+           first_choice = second_choice
+           second_choice = random.choice(game_data.data)
+         else:
+           print(f"\nyou are wrong ! \n{second_choice['name']} has {second_choice['follower_count']} followers")
+           print(f"your score is {score}")
+           game_over = True
+    else:
+      print("invalid input")
+      game_over = True
 
-option_A_print = print(f"Option A: {first_choice['name']},\na {first_choice['description']} has {first_choice['follower_count']} followers.")
-
-print ("\nvs\n")
-
-option_B_print = print(f"Option B: {second_choice['name']}, a {second_choice['description']}")
-
-higher_or_lower = input("is option A has higher or lower followers than option B ? ")
-
-if higher_or_lower == "higher":
-  if first_choice['follower_count'] > second_choice['follower_count']:
-    print("you are correct")
-    first_choice = second_choice
-    score += 1
+  play_again = input("\nwould you like to play a game? type 'y' or 'n': ").lower()
+  
+  if play_again == "y":
+    clear()
+    new_game()
   else:
-    print("you are wrong")
-    print(f"your score is {score}")
+    print("thank you for playing")
+  
+new_game()
